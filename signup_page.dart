6be130c392bc415edd80b92c105b4600c80dd161
Tini Treesa Joseph <95175982+Tinitreesajoseph@users.noreply.github.com>
2024-04-07 01:39:59 +0530

    Updated signup_page.dart

--- signup_page.dart
+++ signup_page.dart
@@ -8,6 +8,9 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   String _selectedUserType = 'Select'; // Default value
   String _agencyNo = ''; // Agency number field value
+  String _email = '';
+  String _password = '';
+  String _confirmPassword = '';
   bool _showAgencyField = false; // Whether to show the agency number field
 
   @override
@@ -23,112 +26,173 @@ class _SignupPageState extends State<SignupPage> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: screenWidth * 0.7,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: screenWidth * 0.7,
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: 'Age',
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: screenWidth * 0.7,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'District',
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: screenWidth * 0.7,
-                child: DropdownButton<String>(
-                  value: _selectedUserType,
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _selectedUserType = newValue!;
-                      if (_selectedUserType == 'Seller') {
-                        _showAgencyField = true;
-                      } else {
-                        _showAgencyField = false;
-                      }
-                    });
-                  },
-                  items: <String>['Select', 'Seller', 'Buyer']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-              ),
-              SizedBox(height: 20),
-              if (_showAgencyField)
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
                 Container(
                   width: screenWidth * 0.7,
                   child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                    ),
                     onChanged: (value) {
                       setState(() {
-                        _agencyNo = value;
+                        _email = value;
                       });
                     },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: screenWidth * 0.7,
+                  child: TextFormField(
+                    obscureText: true,
                     decoration: InputDecoration(
-                      labelText: 'Agency Number',
+                      labelText: 'Password',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _password = value;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: screenWidth * 0.7,
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _confirmPassword = value;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: screenWidth * 0.7,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Age',
                     ),
                   ),
                 ),
-              SizedBox(height: 20),
-              Container(
-                width: screenWidth * 0.7,
-                child: ElevatedButton(
-                     onPressed: () {
+                SizedBox(height: 20),
+                Container(
+                  width: screenWidth * 0.7,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'District',
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: screenWidth * 0.7,
+                  child: DropdownButton<String>(
+                    value: _selectedUserType,
+                    onChanged: (String? newValue) {
                       setState(() {
+                        _selectedUserType = newValue!;
                         if (_selectedUserType == 'Seller') {
-                           Navigator.pushNamed(context, '/seller');
-                        } else if (_selectedUserType == 'Buyer') {
-                           Navigator.pushNamed(context, '/buyer');
-                          }
+                          _showAgencyField = true;
+                        } else {
+                          _showAgencyField = false;
+                        }
+                      });
+                    },
+                    items: <String>['Select', 'Seller', 'Buyer']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                if (_showAgencyField)
+                  Container(
+                    width: screenWidth * 0.7,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          _agencyNo = value;
                         });
                       },
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Color(0xFF04004B)),
+                      decoration: InputDecoration(
+                        labelText: 'Agency Number',
+                      ),
+                    ),
                   ),
+                SizedBox(height: 20),
+                Container(
+                  width: screenWidth * 0.7,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate input fields
+                      if (_email.isNotEmpty &&
+                          _password.isNotEmpty &&
+                          _confirmPassword.isNotEmpty &&
+                          _password == _confirmPassword) {
+                        // Proceed with signup
+                        if (_selectedUserType == 'Seller') {
+                          Navigator.pushNamed(context, '/seller');
+                        } else if (_selectedUserType == 'Buyer') {
+                          Navigator.pushNamed(context, '/buyer');
+                        }
+                      } else {
+                        // Show error message
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Text('Error'),
+                              content: Text('Please fill all fields and ensure passwords match.'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('OK'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xFF04004B)),
+                    ),
+                    child: Text(
+                      'Signup',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
                   child: Text(
-                    'Signup',
-                    style: TextStyle(color: Colors.white),
+                    "Already have an account? Login",
+                    style: TextStyle(
+                      color: Colors.black,
+                      decoration: TextDecoration.underline,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, '/login');
-                },
-                child: Text(
-                  "Already have an account? Login",
-                  style: TextStyle(
-                    color: Colors.black,
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
