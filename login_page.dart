509486695f63577534c384f45c5cc007dbe42b6f
Tini Treesa Joseph <95175982+Tinitreesajoseph@users.noreply.github.com>
2024-04-07 01:45:23 +0530

    Updated login_page.dart

--- login_page.dart
+++ login_page.dart
@@ -7,7 +7,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String _selectedUserType = 'Select'; // Default value
+  String _selectedUserType = 'UserType'; // Default value
 
   @override
   Widget build(BuildContext context) {
